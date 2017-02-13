ed4d3c50c8e788a16b8ce1bbc6e8734047bf1ad7
Rodolfo Guichón <guichonr@gmail.com>
2017-02-13 10:16:04 +0100

    JSON test

--- src/main/java/ai/wide/MyResource.java
+++ src/main/java/ai/wide/MyResource.java
@@ -1,9 +1,12 @@
 package ai.wide;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -18,8 +21,14 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Hello, Heroku!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIt() {
+        
+        JsonObject value = Json.createObjectBuilder()
+                .add("firstName", "Jeremy")
+                .add("lastName", "Chung")
+                .add("message", "Hello World!")
+                .build();
+        return Response.status(200).entity(value).build();
     }
 }
